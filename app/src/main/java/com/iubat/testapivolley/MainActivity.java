a937dfafdbccca48d624d32f61ea720fb1fd8f26
mh-rabbi <mhr221official@gmail.com>
2025-04-25 01:31:31 +0600

    volley api setting-2(done fetching data request & showing response

--- app/src/main/java/com/iubat/testapivolley/MainActivity.java
+++ app/src/main/java/com/iubat/testapivolley/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView responseText;
     private ProgressBar progressBar;
     private Button fetchButton;
 
-    private static final String API_URL = "https://api.example.com/data";
+    private static final String API_URL = "https://official-joke-api.appspot.com/random_joke";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         fetchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                fetchDataFromApi();
             }
         });
     }
@@ -39,9 +47,26 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
         responseText.setText("");
 
+        //before adding volley it was showing error but after adding volley
+        // in dependency we can use it for api
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-
-
+        //now let us request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,API_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+                responseText.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+                responseText.setText("Error: " + error.getMessage());
+                Toast.makeText(MainActivity.this, "Error fetching data", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Add the request to the RequestQueue
+        queue.add(stringRequest);
     }
-
 }
